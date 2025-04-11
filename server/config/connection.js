2520b8c53c0567d8048efce225097451f1bc6272
Roopa Thimmanacherla <roopareddy90@gmail.com>
2025-04-10 17:17:41 -0700

    added new mongodb atlas connection uri string

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 
 //mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 mongoose.connect(
-  "mongodb+srv://roopareddy90:Roopa143@cluster0.f9yoeya.mongodb.net/googlebooks?retryWrites=true&w=majority&appName=Cluster0"
+  "mongodb+srv://roopareddy:password1234@cluster0.gaxh4xl.mongodb.net/Book?retryWrites=true&w=majority&appName=Cluster0"
 );
 module.exports = mongoose.connection;
